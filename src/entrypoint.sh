d1f720ecd74f07b2a815cbeba8e5af18c0287fee
Uherkovich Péter <uherkovich.peter@gmail.com>
2025-08-21 22:23:31 +0200

    fix environment

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -10,6 +10,8 @@ if [ "$APP_DB_PASSWORD" == "" ]; then
   echo "Warning: no password is set in APP_DB_PASSWORD neither APP_DB_PASSWORD_FILE ($APP_DB_PASSWORD_FILE)"
 fi
 
+printenv | sed 's/^\([a-zA-Z0-9_]*\)=\(.*\)$/export \1='"'"'\2'"'"'/g' > /root/env.sh
+
 # Replace cron schedule from environment
 if [ "$CRON_SCHEDULE" = "" ]; then
     CRON_SCHEDULE="21 1 * * *"
